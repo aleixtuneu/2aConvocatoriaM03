dc9c0176074c55aea19e4b0a9d416ad5d8af5744
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-06-02 15:57:38 +0200

    feature: Exercise Nine

---
+++ ExerciseNine/ExerciseNine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Convocatoria2
+{
+    /* Ex.9 */
+    public class Program
+    {
+        public static void Main()
+        {
+            int x = 4;
+            int y = 7;
+            bool p = true;
+            bool q = false;
+
+            // Expressió b)
+            bool resultat_b = (x + y) % 3 == 0 || p != q;
+            Console.WriteLine($"Resultat b): {resultat_b}"); // Esperat: True
+
+            // Expressió c)
+            bool resultat_c = !(x * y < 30) && p;
+            Console.WriteLine($"Resultat c): {resultat_c}"); // Esperat: False
+
+            Console.WriteLine("Depuració finalitzada. Prem una tecla per sortir.");
+            Console.ReadKey(); // Punt per posar un breakpoint
+        }
+    }
+}
