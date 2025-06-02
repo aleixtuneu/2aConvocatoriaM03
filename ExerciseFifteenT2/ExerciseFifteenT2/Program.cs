aa0794ce48ad47d176e2b22e8e780d3bdf6cb772
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-06-02 19:25:41 +0200

    feature: exercise fifteen t2

---
+++ ExerciseFifteenT2/ExerciseFifteenT2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Convocatoria2
+{
+    /* 15. Crea una funció que rebi un número i retorni el seu quadrat. Demostra el seu funcionament des del Main. */
+    public class Program
+    {
+        public static void Main()
+        {
+            const string MsgResult = "Resultat de {0} al quadrat: {1}";
+
+            int inputNumber = 2;
+
+            Console.WriteLine(MsgResult, inputNumber, SquareNumber(inputNumber));
+            Console.WriteLine();
+        }
+
+        public static double SquareNumber(int pNumber)
+        {
+            return Math.Pow(pNumber, 2); ;
+        }
+    }
+}
