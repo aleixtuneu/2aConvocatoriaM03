01749e8565448c68793800e48344ed34c927c125
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-06-02 20:00:07 +0200

    feature: exercise eighteen t2

---
+++ ExerciseEighteenT2/ExerciseEighteenT2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Convocatoria2
+{
+    /* 18. Crea un programa que:
+    Demani 5 paraules i les guardi en un array
+    Mostri les paraules en ordre invers
+     */
+    public class Program
+    {
+        public static void Main()
+        {
+            const string MsgIntroduceWord = "Introdueix la paraula [{0}]: ";
+            const string MsgResult = "Paraules en ordre invers: ";
+            const int Size = 5;
+
+            string inputWord = "";
+            string[] inputWords = new string[Size];
+
+            // Demanar 5 paraules
+            for (int i = 0; i < Size; i++)
+            {
+                Console.WriteLine(MsgIntroduceWord, i+1);
+                inputWord = Console.ReadLine();
+                inputWords[i] = inputWord;
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            // Mostrar paraules en ordre invers
+            Console.WriteLine(MsgResult);
+            for (int i = inputWords.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(inputWords[i]);
+            }
+            Console.WriteLine();
+        }
+    } 
+}
