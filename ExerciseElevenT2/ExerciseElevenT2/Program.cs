40949dfcd8b729fcfbf31ff3e74d3d9286616310
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-06-02 18:50:27 +0200

    feature: exercise eleven t2

---
+++ ExerciseElevenT2/ExerciseElevenT2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Convocatoria2
+{
+    /* 11. Demana a l’usuari una data en format dd/MM/yyyy. Mostra si és una data vàlida i en quin dia de la setmana cau. */
+    public class Program
+    {
+        public static void Main()
+        {
+            const string MsgIntroduceDate = "Introdueix una data en el format dd/MM/yyyy: ";
+            const string MsgValidDate = "La data {0} és vàlida.";
+            const string MsgInvalidDate = "La data {0} no és vàlida.";
+            const string MsgResult = "Cau en: {0}";
+
+            string inputDate = "";
+            DateTime validDate = new DateTime();
+            CultureInfo cultureCat = new CultureInfo("ca-Es");
+
+            // Demanar data
+            Console.WriteLine(MsgIntroduceDate);
+            inputDate = Console.ReadLine();
+            Console.WriteLine();
+
+            if (DateTime.TryParseExact(inputDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out validDate))
+            {
+                Console.WriteLine(MsgValidDate);
+
+                // Mostrar dia de la setmana en català
+                Console.WriteLine(MsgResult, validDate.ToString("dddd", cultureCat));
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine(MsgInvalidDate);
+                Console.WriteLine();
+            }
+        }
+    }
+}
